0ad5433ea78f6de9113bffbcc4e6712be52e7dd0
Charlie <eikascharlie@gmail.com>
2023-09-28 17:00:44 +0200

    Setting up basic player controller

--- Plantmania/Assets/_Scripts/PlayerController.cs
+++ Plantmania/Assets/_Scripts/PlayerController.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    [SerializeField] private int speed = 5;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMovement(InputValue inputValue) //this gets the value from the PlayerInputs
     {
-        
+        movement = inputValue.Get<Vector2>(); //WASD
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime); //makes the player move when pressing WASD
     }
 }
