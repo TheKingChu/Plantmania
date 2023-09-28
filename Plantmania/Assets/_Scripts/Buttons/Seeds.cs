657c42d0e4b74ba36bcc480c80c363f2ca0223e4
Charlie <eikascharlie@gmail.com>
2023-09-28 17:50:17 +0200

    Start on inventory system

---
+++ Plantmania/Assets/_Scripts/Buttons/Seeds.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seeds : MonoBehaviour
+{
+    enum SeedType
+    {
+        Basic,
+
+    }
+
+    public void SelectedSeed()
+    {
+        Debug.Log("Working");
+    }
+}
