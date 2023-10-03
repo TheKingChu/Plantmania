732997a3079ece0a2cf4565224fd1202440dc3d6
Charlie <eikascharlie@gmail.com>
2023-10-03 18:16:56 +0200

    starting on drag and drop from inventory

--- Plantmania/Assets/_Scripts/Inventory/Invetory_UI.cs
+++ Plantmania/Assets/_Scripts/Inventory/Invetory_UI.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Invetory_UI : MonoBehaviour
 {
     public GameObject inventoryPanel;
     public PlayerManager player;
     public List<Slot_UI> slots = new List<Slot_UI>();
+   
+    [SerializeField] private Canvas canvas;
+
+    private Slot_UI draggedSlot;
+    private Image draggedIcon;
+
+    private void Awake()
+    {
+        canvas = FindObjectOfType<Canvas>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -57,6 +68,43 @@ public class Invetory_UI : MonoBehaviour
             player.DropItem(itemToDrop);
             player.inventory.Remove(slotID);
             Refresh();
+        }
+    }
+
+    public void SlotBeginDrag(Slot_UI slot)
+    {
+        draggedSlot = slot;
+        draggedIcon = Instantiate(draggedSlot.itemIcon);
+        draggedIcon.transform.SetParent(canvas.transform);
+        draggedIcon.raycastTarget = false;
+
+        draggedIcon.rectTransform.sizeDelta = new Vector2(50, 50);
+
+        MoveToMouse(draggedIcon.gameObject);
+    }
+
+    public void SlotDrag()
+    {
+        MoveToMouse(draggedIcon.gameObject);
+    }
+
+    public void SlotEndDrag()
+    {
+
+    }
+
+    public void SlotDrop(Slot_UI slot)
+    {
+
+    }
+
+    private void MoveToMouse(GameObject toMove)
+    {
+        if(canvas != null)
+        {
+            Vector2 position;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, Input.mousePosition, null, out position);
+            toMove.transform.position = canvas.transform.TransformPoint(position);
         }
     }
 }
